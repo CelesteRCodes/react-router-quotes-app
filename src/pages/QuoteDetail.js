80593c15b18d7ee4f244b62b57bdc5abb92659eb
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-20 15:21:09 -0500

    completed practice app with GET POST to Firebase to add new quotes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,40 +1,85 @@
-import React from "react";
-import { useParams, Route } from "react-router-dom";
+import React, {useEffect} from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-  {
-    id: 'q1',
-    author: 'Celeste',
-    text: 'It is what it is'
-  },
-  {
-    id: 'q2',
-    author: 'Shan',
-    text: 'Dems the breaks'
-  }
-];
+// const DUMMY_QUOTES = [
+//   {
+//     id: 'q1',
+//     author: 'Celeste',
+//     text: 'It is what it is'
+//   },
+//   {
+//     id: 'q2',
+//     author: 'Shan',
+//     text: 'Dems the breaks'
+//   }
+// ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
+
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(
-    quote => quote.id === params.quoteId
-  );
+  const { quoteId } = params;
 
-  if (!quote) {
-    return <p>No quote found</p>
-  };
+  const {sendRequest, status, data: loadedQuote, error} = useHttp(
+    getSingleQuote, 
+    true);
 
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  // const quote = DUMMY_QUOTES.find(
+  //   quote => quote.id === params.quoteId
+  // );
+
+  if (status === 'pending') { 
+    return (
+      <div className='centered'>
+      <LoadingSpinner />
+    </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered'>{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No Quote Found!</p>
+  }
+
+  // if (!quote) {
+  //   return <p>No quote found</p>
+  // };
+
+  // match this url and add /comments to go to comments page
+  // `${match.path}`
+  
   return (
     <>
       <h1>Quote Detail Page </h1>
       <HighlightedQuote
-        text={quote.text}
-        author={quote.author}
+        text={loadedQuote.text}
+        author={loadedQuote.author}
       />
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link
+            className='btn--flat'
+            to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </>
